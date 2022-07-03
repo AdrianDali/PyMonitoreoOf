3918a213ed0d40070fa11cf49c3122c1df1422c5
AdrianDali <hadriandali@gmail.com>
2022-07-03 01:51:02 -0500

    commit  1

---
+++ src/model/mysql.py
@@ -0,0 +1,18 @@
+import datetime 
+from ast import Try 
+import pymysql
+
+class DataBase:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            user = 'root',
+            passwd = '1234',
+            db = 'monitoreo_trabajo'
+            )
+        self.cursor = self.connection.cursor()
+        print('Conexion exitosa')
+        return self.cursor
+
+    
+
